e4c33fc5e2566003d7af5d6a0e101ec02358a63c
Srinath Hariharan <shacstudios@gmail.com>
2023-12-08 17:50:31 -0800

    have run build

---
+++ MyBot/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Mybot from './Mybotbckend';
+
+const App = ({ assistantId, title, theme }) => {
+  const [collapsed, setCollapsed] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [localAssistantId, setLocalAssistantId] = useState('');
+
+  useEffect(() => {
+    // 1. Get the script tag element
+    const scriptTag = document.querySelector('script[src*="BotEmbed.js"]');
+  
+    // 2. Extract configuration attributes
+    // 3. Initialize the chat bot with the extracted configuration
+    // Update the state accordingly
+    setLocalAssistantId(assistantId);
+  
+    // ...
+  
+  }, []);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+
+  return (
+    <div className="chatbot-embed">
+      <Mybot
+        assistantId={assistantId}
+        title={title}
+        theme={theme}
+      />
+    </div>
+  );
+
+};
+
+
+export default App;
